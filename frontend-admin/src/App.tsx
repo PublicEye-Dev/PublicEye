d5732eef4eb89b41c4dd43c27829da3dbbffeb99
Roxana Daraban <roxana.daraban@student.upt.ro>
2025-11-15 02:32:08 +0200

    added viewdepartment component and page, env in frontend-admin

--- frontend-admin/src/App.tsx
+++ frontend-admin/src/App.tsx
@@ -1,3 +1,14 @@
+import ViewDepartmentPage from "./Pages/ViewDepartmentPage/ViewDepartmentPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+
+
 export default function App() {
-  return <div>PublicEye App</div>;
+
+  return( <BrowserRouter>
+      <Routes>
+        <Route path="/departament-admin" element={<ViewDepartmentPage/>} />
+      </Routes>
+    </BrowserRouter>)
 }
