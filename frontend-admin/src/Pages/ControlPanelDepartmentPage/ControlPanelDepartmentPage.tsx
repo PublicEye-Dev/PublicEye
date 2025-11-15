83035fcf3c7cdc193ecf4acc5b6ff429a821037f
Roxana Daraban <roxana.daraban@student.upt.ro>
2025-11-15 15:07:57 +0200

    added button in controlpaneldepartamentpage

--- frontend-admin/src/Pages/ControlPanelDepartmentPage/ControlPanelDepartmentPage.tsx
+++ frontend-admin/src/Pages/ControlPanelDepartmentPage/ControlPanelDepartmentPage.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import Navbar from "../../Components/Layout/Navbar/Navbar";
 import CardControlPanel from "../../Components/CardControlPanel/CardControlPanel";
-import { 
-    FaShieldAlt,    
-    FaMoneyBillWave, 
-    FaCity,          
-} from "react-icons/fa";
+import { FaShieldAlt, FaMoneyBillWave, FaCity } from "react-icons/fa";
 import "./ControlPanelDepartmentPage.css";
 
 const ControlPanelDepartamentPage: React.FC = () => {
-
   return (
     <div className="page-container">
       <div className="page-navbar">
@@ -17,49 +12,55 @@ const ControlPanelDepartamentPage: React.FC = () => {
       </div>
 
       <div className="control-panel-container">
-        
-        <div className="control-panel-grid"> 
+        <div className="add-department-container">
+          <div className="title">
+            <h4>Gestionare departamente</h4>
+          </div>
+          <button className="add-department-button">Adaugă departament</button>
+        </div>
+
+        <div className="control-panel-grid">
+          <CardControlPanel
+            icon={<FaShieldAlt />}
+            title="Poliție"
+            description="Vezi rapoartele de poliție"
+            to="/politie"
+          />
 
           <CardControlPanel
-              icon={<FaShieldAlt />}
-              title='Poliție'
-              description='Vezi rapoartele de poliție'
-              to='/politie'
-            />
+            icon={<FaMoneyBillWave />}
+            title="Taxe"
+            description="Vezi rapoartele de taxe"
+            to="/taxe"
+          />
 
-            <CardControlPanel
-              icon={<FaMoneyBillWave />}
-              title='Taxe'
-              description='Vezi rapoartele de taxe'
-              to='/taxe'
-            />
+          <CardControlPanel
+            icon={<FaCity />}
+            title="Urbanism"
+            description="Vezi rapoartele de urbanism"
+            to="/urbanism"
+          />
 
-            <CardControlPanel
-              icon={<FaCity />}
-              title='Urbanism'
-              description='Vezi rapoartele de urbanism'
-              to='/urbanism'
-            />
-            <CardControlPanel
-              icon={<FaShieldAlt />}
-              title='Poliție'
-              description='Vezi rapoartele de poliție'
-              to='/politie'
-            />
+          <CardControlPanel
+            icon={<FaShieldAlt />}
+            title="Poliție"
+            description="Vezi rapoartele de poliție"
+            to="/politie"
+          />
 
-            <CardControlPanel
-              icon={<FaMoneyBillWave />}
-              title='Taxe'
-              description='Vezi rapoartele de taxe'
-              to='/taxe'
-            />
+          <CardControlPanel
+            icon={<FaMoneyBillWave />}
+            title="Taxe"
+            description="Vezi rapoartele de taxe"
+            to="/taxe"
+          />
 
-            <CardControlPanel
-              icon={<FaCity />}
-              title='Urbanism'
-              description='Vezi rapoartele de urbanism'
-              to='/urbanism'
-            />
+          <CardControlPanel
+            icon={<FaCity />}
+            title="Urbanism"
+            description="Vezi rapoartele de urbanism"
+            to="/urbanism"
+          />
         </div>
       </div>
     </div>
